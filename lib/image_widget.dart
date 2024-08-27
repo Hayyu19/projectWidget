effbfb19201794893cc9fdbd6dfe351e2cae2772
Hayyu19 <hayyuarema@gmail.com>
2024-08-27 09:07:58 +0700

    Update image_widget.dart

--- lib/image_widget.dart
+++ lib/image_widget.dart
@@ -5,23 +5,15 @@ class ImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-//    return SingleChildScrollView(
-//      child: Column(
-//        children: [
-//          Image(
-//            image: NetworkImage(
-//            'https://cdn-web.ruangguru.com/landing-pages/assets/hs/Sejarah_11.jpg'
-//            )
-//            ),
-//            Image(
-//            image: NetworkImage(
-//            'https://cdn-web.ruangguru.com/landing-pages/assets/hs/Sejarah_11.jpg'
-//            )
-//            ),
-//      
-//        ],
-//      ),
-//    );
-      return Image(image: AssetImage('assets/download.png'));
+   
+     return SingleChildScrollView(
+      child: Column(
+        children: [
+          Image(image: AssetImage('costa.jpg')),  
+          Image(image: AssetImage('11.jpg'))        
+        ],
+      ),
+  );
+   
   }
 }
